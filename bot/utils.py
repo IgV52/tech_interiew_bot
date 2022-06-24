c055ae479b0bc71276b5a7ab0ea19dab37f2091e
Ivan Grubov <qalbertooq@mail.ru>
2022-06-25 00:51:03 +0300

    fix type

--- bot/utils.py
+++ bot/utils.py
@@ -3,7 +3,7 @@ from bot import constants
 from bot import settings
 
 def check_role(user_id):
-    if user_id in settings.ADMIN:
+    if str(user_id) in settings.ADMIN:
         return constants.ADMIN
     return constants.USER
 
